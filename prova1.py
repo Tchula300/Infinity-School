e32517a353cbf221ddae89f3473a41349fd1864f
Tchula300 <josantossilva63@gmail.com>
2024-12-14 11:08:16 -0300

    Add files via upload
    São as provas do curso na infinity school.

---
+++ prova1.py
@@ -0,0 +1,15 @@
+#[LPIA-A01] Você está criando um programa para calcular a área de um retângulo. 
+#O programa deve solicitar ao usuário que forneça a base e a altura do retângulo. 
+#Em seguida, o programa deve calcular a área usando a fórmula:
+
+#Área=Base×Altura
+
+altura = float(input('Digite a altura do retângulo: '))
+base = float(input('Digite a base do retângulo: '))
+
+area = base * altura
+
+print(f'A área do retângulo é: {area}')
+
+
+
---
+++ prova2.py
@@ -0,0 +1,17 @@
+#[LPIA-A02] Crie um programa em Python para verificar se um número é positivo, 
+# negativo ou zero. O programa deve solicitar ao usuário que insira um número e, 
+# em seguida, imprimir uma mensagem indicando a natureza do número.
+
+#Se o número for maior que zero, exiba a mensagem "O número é positivo.
+# " Se for menor que zero, exiba "O número é negativo." Caso seja zero, a mensagem deve ser "O número é zero."
+
+num = int(input('Digite um número: '))
+
+if num > 0:
+    print('O número é positivo.')
+
+elif num < 0:
+    print('O número é negativo.')
+
+else:
+    print('O número é zero.')
